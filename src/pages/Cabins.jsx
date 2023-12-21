1bea34b871179017ea18d456aa0797cbb4b2b1c4
Sarath <cheekatisarath@gmail.com>
2023-12-21 16:30:35 +0530

    till 360

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,10 +4,9 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import Button from "../ui/Button";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
-
   return (
     <>
       <Row type="horizontal">
@@ -17,11 +16,7 @@ function Cabins() {
 
       <Row>
         <CabinTable />
-
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add new cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
